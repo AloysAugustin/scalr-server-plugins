fef19aff9619ef079c8308948eb62ff672db7f43
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-03-08 17:48:58 +0100

    Test virtualenv and settings in example plugin

--- samples/testplugin/wsgi.py
+++ samples/testplugin/wsgi.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
+
+import json
+import Flask # dependency test
+import os
 import sys
 import test_module
+
+settings = {}
+
 def application(environ, start_response):
     status = '200 OK'
-    output = 'Hello World!\n'
+    output = 'Hello {}!\n'.format(settings.get('NAME'))
     output = output + str(environ) + '\n' +  str(test_module.blah()) + '\n' + str(sys.path) + '\n'
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
     return [output]
+
+config_file = os.path.join(__file__, 'settings.json')
+with open(config_file) as f:
+    settings = json.loads(f.read())
